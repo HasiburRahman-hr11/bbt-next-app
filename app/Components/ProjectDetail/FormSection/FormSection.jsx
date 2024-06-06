daaeb2e4366613c939ded38415b0649d2b766546
HasiburRahman-hr11 <mdhasiburrahman47@gmail.com>
2024-06-06 17:43:23 +0600

    spacing

--- app/Components/ProjectDetail/FormSection/FormSection.jsx
+++ app/Components/ProjectDetail/FormSection/FormSection.jsx
@@ -10,14 +10,14 @@ const FormSection = () => {
       <div className="max-1920">
         <div className="container">
           <div className="xl:flex xl:items-center">
-            <div className="xl:w-[30%] 1xl:w-[33%] 3xl:w-[30%]">
+            <div className="xl:w-[30%] 1xl:w-[32%] 3xl:w-[30%]">
               <h4 className="xl:text-[2.2rem] 1xl:text-[2.4rem] 2xl:text-[2.5rem] 3xl:text-[3rem] 3xl:tracking-tight">Looking For Something Similar?</h4>
               <p className="font-light mt-[1rem] xl:text-[1.3rem] 1xl:text-[1.4rem] 2xl:text-[1.5rem] 3xl:text-[1.65rem]">
                 Drop us your detail and our team will get back to you with the
                 info.
               </p>
             </div>
-            <div className="mt-[4rem] xl:mt-0 xl:flex-[1] xl:pl-[3rem] 1xl:pl-[5rem]">
+            <div className="mt-[4rem] xl:mt-0 xl:flex-[1] xl:pl-[3rem] 1xl:pl-[3rem] 3xl:pl-[5rem]">
               <form action="" className="block">
                 <div className="md:flex md:justify-between">
                   <div className="mb-[1.5rem] md:w-[25%]">
@@ -66,6 +66,6 @@ const FormSection = () => {
 export default FormSection;
 
 const styles = {
-    inputStyles:'w-full bg-transparent border border-[#AAAAAA] outline-none rounded-[0.7rem] text-[1.5rem] 1xl:text-[1.6rem] 2xl:text-[1.7rem] 3xl:text-[1.8rem] px-[1rem] py-[1rem] h-[5rem] 1xl:h-[5.5rem] 2xl:h-[6rem] 3xl:h-[7rem] 1xl:rounded-[1rem]',
-    buttonStyle: 'w-full bg-black text-white border border-black outline-none rounded-[0.7rem] text-[1.5rem] 1xl:text-[1.6rem] 2xl:text-[1.7rem] 3xl:text-[1.8rem] px-[1rem] py-[1rem] h-[5rem] 1xl:h-[5.5rem] 2xl:h-[6rem] 3xl:h-[7rem] 1xl:rounded-[1rem] hover:bg-[#111111]'
+    inputStyles:'w-full bg-transparent border border-[#AAAAAA] outline-none rounded-[0.7rem] text-[1.5rem] 1xl:text-[1.5rem] 2xl:text-[1.6rem] 3xl:text-[1.8rem] px-[1rem] py-[1rem] h-[5rem] 1xl:h-[5.5rem] 2xl:h-[6rem] 3xl:h-[7rem] 1xl:rounded-[1rem] 1xl:px-[2rem]',
+    buttonStyle: 'w-full bg-black text-white border border-black outline-none rounded-[0.7rem] text-[1.5rem] 1xl:text-[1.5rem] 2xl:text-[1.6rem] 3xl:text-[1.8rem] px-[1rem] py-[1rem] h-[5rem] 1xl:h-[5.5rem] 2xl:h-[6rem] 3xl:h-[7rem] 1xl:rounded-[1rem] hover:bg-[#111111]'
 }
