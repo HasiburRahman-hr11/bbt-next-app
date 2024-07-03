c2dd8677a46affabb36af58e9220693ce7fa21ce
HasiburRahman-hr11 <mdhasiburrahman47@gmail.com>
2024-07-03 17:29:24 +0600

    presence section desktop

--- app/Components/WhyUs/OurPresence/OurPresence.jsx
+++ app/Components/WhyUs/OurPresence/OurPresence.jsx
@@ -1,18 +1,67 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 
+const showrooms = [
+  {
+    title: "Delhi/NCR (HQ)",
+    address:
+      "Plot No. 134, Sector 37, Pace City 1, Gurgaon Haryana, 122001, India",
+    icon: "/images/why-us/showroom-icon-1.webp",
+    thumbnail: "/images/why-us/showroom-4.webp",
+  },
+  {
+    title: "Mumbai",
+    address:
+      "7, Hubtown Sunmist, Opposite Hubtown Solaris, Telli Galli, Andheri East, N S Phadke Marg, Mumbai - 400053",
+    icon: "/images/why-us/showroom-icon-2.webp",
+    thumbnail: "/images/why-us/showroom-4.webp",
+  },
+  {
+    title: "Hydrebad",
+    address:
+      "Road no 2, Banjara Hills, Shangrila Plaza, Opp.KBR Park, Hyderabad, Telangana - 500034",
+    icon: "/images/why-us/showroom-icon-3.webp",
+    thumbnail: "/images/why-us/showroom-4.webp",
+  },
+  {
+    title: "Ahmedabad Studio",
+    address:
+      "Cama Motors, Rustom Cama Marg, Old City, Lal Darwaja, Ahmedabad, Gujarat 380001",
+    icon: "/images/why-us/showroom-icon-4.webp",
+    thumbnail: "/images/why-us/showroom-4.webp",
+  },
+  {
+    title: "Bangalore",
+    address:
+      "140, Residency Rd, Shanthala Nagar, Richmond Town, Bengaluru, Karnataka 560025",
+    icon: "/images/why-us/showroom-icon-5.webp",
+    thumbnail: "/images/why-us/showroom-4.webp",
+  },
+  {
+    title: "Big Boy Toyz | Ludhiana",
+    address:
+      "1st Floor, Lake Street, Ferozepur - Ludhiana Rd, Dakha, Punjab 141102",
+    icon: "/images/why-us/showroom-icon-6.webp",
+    thumbnail: "/images/why-us/showroom-4.webp",
+  },
+];
+
 const OurPresence = () => {
-  const [expandedColumn, setExpandedColumn] = useState(0); // Default to the first column
+  const [expandedColumn, setExpandedColumn] = useState(0);
+  const [showText, setShowText] = useState(0);
 
   const handleColumnClick = (index) => {
     setExpandedColumn(index);
+    setTimeout(() => {
+      setShowText(index);
+    }, 400);
   };
   return (
     <section className="bg-[#F4F4F1] py-[6rem] lg:py-[8rem] xl:py-[12rem] 1xl:py-[14rem] 3xl:py-[18rem]">
       <div className="max-1920">
         <div className="container">
-          <div className="flex flex-wrap">
-            <div className="w-full text-center">
+          <div className="flex flex-wrap lg:items-center">
+            <div className="w-full text-center lg:flex-[1] lg:w-max lg:text-left">
               <h2 className="text-[2.9rem] font-light [&>b]:font-normal leading-[1.1] tracking-[-0.1rem] mb-[2rem] xl:text-[3.7rem] 1xl:text-[4.2rem] 2xl:text-[4.4rem] 3xl:text-[5.8rem] capitalize [&>br]:hidden md:[&>br]:block">
                 our presence and <b>infrastructure</b>
               </h2>
@@ -21,31 +70,78 @@ const OurPresence = () => {
                 range of and available car models in the market.
               </p>
             </div>
-            <div className="w-full flex justify-center items-center">
+            <div className="w-max hidden lg:block">
               <img
                 src="/images/down-circle-arrow-white.webp"
                 width="123"
                 height="123"
                 alt="Arrow Icon"
-                className="hidden xl:inline-block object-contain invert xl:w-[8.5rem] xl:mt-[4rem] 3xl:mt-[6rem] 1xl:w-[9rem] 2xl:w-[9.5rem] 3xl:w-[12.36rem]"
+                className="object-contain invert lg:w-[7rem] xl:w-[8.5rem] xl:mt-[4rem] 3xl:mt-[6rem] 1xl:w-[9rem] 2xl:w-[9.5rem] 3xl:w-[12.36rem]"
               />
             </div>
           </div>
 
-          <div>
-            {/* <div className="flex w-full">
-              {Array.from({ length: 6 }, (_, index) => (
+          <div className="flex w-full flex-wrap justify-between mt-[5rem] lg:mt-[7rem] lg:w-[101%] lg:mx-auto">
+            {showrooms.map((showroom, index) => (
+              <div
+                key={index}
+                className={`lg:flex-[0.7] transition-all duration-500 relative overflow-hidden lg:px-[1rem] xl:px-[0.8rem] 3xl:px-[1.2rem] ${
+                  expandedColumn === index ? "lg:flex-[3]" : ""
+                }`}
+                onClick={() => handleColumnClick(index)}
+              >
                 <div
-                  key={index}
-                  className={`flex-[0.7] transition-all duration-500 bg-[#ddd] ${
-                    expandedColumn === index ? "flex-[3]" : ""
+                  className={`relative overflow-hidden h-full w-full cursor-pointer lg:h-[36rem] xl:h-[48rem] 1xl:h-[57rem] 3xl:h-[73rem] ${
+                    expandedColumn === index
+                      ? "lg:rounded-[3.5rem] xl:rounded-[6rem] 3xl:rounded-[8rem]"
+                      : "lg:rounded-[5rem] xl:rounded-[7rem] 3xl:rounded-[9rem]"
                   }`}
-                  onClick={() => handleColumnClick(index)}
                 >
-                  Column {index + 1}
+                  <img
+                    src={showroom.thumbnail}
+                    alt={showroom.title}
+                    className="w-full h-full object-cover block"
+                  />
+                  <div
+                    className={`absolute w-full h-full left-0 top-0 flex items-end overflow-hidden transition-all duration-500 bg-gradient-to-t from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.01] ${
+                      expandedColumn === index
+                        ? "lg:p-[3rem] xl:p-[3.5rem] 1xl:p-[4rem] 3xl:p-[5.5rem]"
+                        : "lg:p-[1rem] xl:p-[1.5rem] 1xl:pb-[2rem] 3xl:pb-[2.5rem]"
+                    }`}
+                  >
+                    <div
+                      className={`flex relative overflow-hidden w-full items-start h-auto transition-all duration-500 ${
+                        expandedColumn === index
+                          ? "lg:justify-start lg:min-w-[280px] xl:min-w-[380px] 1xl:min-w-[450px] 3xl:min-w-[585px]"
+                          : "lg:justify-center min-w-none"
+                      }`}
+                    >
+                      <div className="lg:w-[6rem] lg:h-[6rem] xl:w-[7rem] xl:h-[7rem] rounded-full bg-[#F4F4F1] flex justify-center items-center p-[1rem] 1xl:p-[1.5rem] transition-all duration-500 1xl:w-[8rem] 1xl:h-[8rem] 3xl:w-[11rem] 3xl:h-[11rem] 3xl:p-[2rem]">
+                        <img
+                          src={showroom.icon}
+                          alt="Icon"
+                          className="w-full h-full object-contain"
+                        />
+                      </div>
+                      <div
+                        className={`lg:flex-[1] pl-[1rem] text-white transition-all duration-500 1xl:pl-[2rem] ${
+                          showText === index ? "lg:opacity-100 block " : ""
+                        } ${
+                          expandedColumn !== index ? "lg:opacity-0 hidden lg:absolute lg:left-[6rem]" : ""
+                        }`}
+                      >
+                        <h6 className="lg:text-[1.6rem] xl:text-[1.8rem] 1xl:text-[2rem] 3xl:text-[3rem]">
+                          {showroom.title}
+                        </h6>
+                        <p className="lg:text-[1.2rem] xl:text-[1.3rem] 1xl:text-[1.4rem] 3xl:text-[2rem] mt-[0.5rem] ">
+                          {showroom.address}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              ))}
-            </div> */}
+              </div>
+            ))}
           </div>
         </div>
       </div>
